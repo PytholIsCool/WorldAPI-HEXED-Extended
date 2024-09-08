62e5776dc30041b7fb1fbc32284f96171e2e5903
Pythol <95198306+PytholIsCool@users.noreply.github.com>
2024-09-07 21:44:44 -0500

    Update QMCToggle.cs
    added toggle control
    
    more difficult since these arent native to vrc
    (yes its a completely custom control)

--- WorldAPI-HEXED-Extension/ButtonAPI/QM/Carousel/Items/QMCToggle.cs
+++ WorldAPI-HEXED-Extension/ButtonAPI/QM/Carousel/Items/QMCToggle.cs
@@ -1,96 +1,170 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using TMPro;
-using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.UI;
 using VRC.Localization;
-using VRC.UI.Element;
 using VRC.UI.Elements.Controls;
-using VRC.UI.Elements.Tooltips;
 using WorldAPI.ButtonAPI.Controls;
-using WorldAPI;
-using Object = UnityEngine.Object;
+using WorldAPI.ButtonAPI.Extras;
 using WorldAPI.ButtonAPI.Groups;
-using Valve.VR.InteractionSystem;
+using Object = UnityEngine.Object;
 
 namespace WorldAPI.ButtonAPI.QM.Carousel.Items
 {
-    public class QMCToggle : ToggleControl //good chunk of this was taken from the CToggle class.
+    public class QMCFuncButton : ExtentedControl //this control was extra difficult for no good fucking reason
     {
-        public Action<bool> ListenerC { get; set; }
-        public Action<bool> SecondaryListener {  get; set; } //this will be used later on...
-        public UiToggleTooltip ToolTip { get; private set; }
-        public UiToggleTooltip SecondaryToolTip { get; private set; } //this will be used later on...
-        public Transform AdditionalToggle { get; set; } //this will be used later on...
-        public Transform Handle { get; private set; }
-        public Transform SecondaryHandle { get; private set; } //this will be used later on...
-//oh also, when i say that those will be used later on, i mean ill be deleting those and doing something else thats way more optimised!
-        private RadioButton toggleSwitch;
+        public static Action<bool> Listener {  get; set; }
+        public static bool isToggled { get; private set; }
+        public static Image ToggleSprite;
+        public static Transform ButtonParent {  get; private set; }
+        public static Transform leftPar {  get; private set; }
+        public static Transform rightPar { get; private set; }
+        public static Sprite OnSprite { get; private set; }
+        public static Sprite OffSprite { get; private set; }
         private bool shouldInvoke = true;
-
-        private static Vector3 onPos = new(93, 0, 0), offPos = new(30, 0, 0);
-        public QMCToggle(Transform parent, string text, string tooltip, Action<bool> stateChange, bool defaultState = false, bool separator = false)
+        public QMCFuncButton(Transform parent, string text, string tooltip, Action listener, bool rightContainer = false, bool separator = false, Sprite sprite = null)
         {
             if (!APIBase.IsReady())
                 throw new NullReferenceException("Object Search had FAILED!");
+            transform = Object.Instantiate(APIBase.QMCarouselFuncButtonTemplate, parent).transform;
+            gameObject = transform.gameObject;
+            gameObject.name = text + "_ControlContainer";
+            leftPar = transform.Find("LeftItemContainer");
+            leftPar.gameObject.DestroyChildren();
+            rightPar = transform.Find("RightItemContainer");
+            rightPar.gameObject.DestroyChildren();
 
-            gameObject = Object.Instantiate(APIBase.QMCarouselToggleTemplate, parent);
-            transform = gameObject.transform;
-            gameObject.name = text;
+            ButtonParent = rightContainer ? rightPar : leftPar;
 
-            TMProCompnt = transform.Find("LeftItemContainer/Title").GetComponent<TextMeshProUGUI>();
+            transform.Find("TitleMainContainer").gameObject.SetActive(false);
+
+            Transform button = Object.Instantiate(APIBase.QMCarouselFuncButtonTemplate.transform.Find("LeftItemContainer/Button (1)"), ButtonParent);
+            button.name = text;
+
+            if(sprite != null)
+            {
+                var Tsprite = button.Find("Icon").GetComponent<Image>();
+                Tsprite.overrideSprite = sprite;
+                Tsprite.gameObject.SetActive(true);
+            }
+
+            TMProCompnt = button.Find("Text_MM_H3").GetComponent<TextMeshProUGUI>();
             TMProCompnt.text = text;
             TMProCompnt.richText = true;
-            Text = text;
-
-            (ToolTip = gameObject.GetComponent<UiToggleTooltip>())._localizableString = tooltip.Localize();
 
             if (separator != false)
             {
                 GameObject seB = Object.Instantiate(APIBase.QMCarouselSeparator, parent);
                 seB.name = "Separator";
             }
-            toggleSwitch = transform.Find("RightItemContainer/Cell_MM_OnOffSwitch").GetComponent<RadioButton>();
-            toggleSwitch.Method_Public_Void_Boolean_0(defaultState);
 
-            (Handle = toggleSwitch._handle)
-                .transform.localPosition = defaultState ? onPos : offPos;
+            button.GetComponent<ToolTip>()._localizableString = tooltip.Localize();
 
-            ToggleCompnt = gameObject.GetComponent<Toggle>();
-            ToggleCompnt.onValueChanged = new();
-            ToggleCompnt.isOn = defaultState;
-            ListenerC = stateChange;
-            ToggleCompnt.onValueChanged.AddListener(new Action<bool>((val) => {
-                if (shouldInvoke)
-                    APIBase.SafelyInvolk(val, ListenerC, text);
-                APIBase.Events.onQMCToggleValChange?.Invoke(this, val);
-                toggleSwitch.Method_Public_Void_Boolean_0(val);
-                Handle.localPosition = val ? onPos : offPos;
-            }));
-            gameObject.GetComponent<SettingComponent>().enabled = false;
+            ButtonCompnt = button.GetComponent<Button>();
+            ButtonCompnt.onClick = new();
+            ButtonCompnt.onClick.AddListener(listener);
+
+            button.gameObject.SetActive(true);
+
+            ButtonParent = null;
+            ButtonCompnt = null;
         }
-        public QMCToggle SetState(bool value)
+        public QMCFuncButton AddButton(string text, string tooltip, Action listener, bool rightContainer = false, Sprite sprite = null)
         {
-            shouldInvoke = false;
-            ToggleCompnt.isOn = value;
-            shouldInvoke = true;
+            ButtonParent = rightContainer ? rightPar : leftPar;
+
+            Transform newButton = Object.Instantiate(APIBase.QMCarouselFuncButtonTemplate.transform.Find("LeftItemContainer/Button (1)"), ButtonParent);
+            newButton.name = text;
+
+            if (sprite != null)
+            {
+                var Tsprite = newButton.Find("Icon").GetComponent<Image>();
+                Tsprite.overrideSprite = sprite;
+                Tsprite.gameObject.SetActive(true);
+            }
+
+            TMProCompnt = newButton.Find("Text_MM_H3").GetComponent<TextMeshProUGUI>();
+            TMProCompnt.text = text;
+            TMProCompnt.richText = true;
+
+            newButton.GetComponent<ToolTip>()._localizableString = tooltip.Localize();
+
+            ButtonCompnt = newButton.GetComponent<Button>();
+            ButtonCompnt.onClick = new();
+            ButtonCompnt.onClick.AddListener(listener);
+
+            newButton.gameObject.SetActive(true);
+
+            ButtonParent = null;
+            ButtonCompnt = null;
+            return this;
+        }
+        public QMCFuncButton AddToggle(string text, string tooltip, Action<bool> listener, bool rightContainer = false, bool defaultState = false, Sprite onSprite = null, Sprite offSprite = null)
+        {
+            ButtonParent = rightContainer ? leftPar : rightPar;
+
+            Transform newToggle = Object.Instantiate(APIBase.QMCarouselFuncButtonTemplate.transform.Find("LeftItemContainer/Button (1)"), ButtonParent);
+            newToggle.name = text + "_FunctionToggle";
+
+            ToggleSprite = newToggle.Find("Icon").GetComponent<Image>();
+            if (onSprite != null) { OnSprite = onSprite; }
+            else { OnSprite = APIBase.OnSprite; }
+
+            if (offSprite != null) { OffSprite = offSprite; }
+            else { OffSprite = APIBase.OffSprite; }
+
+            TMProCompnt = newToggle.Find("Text_MM_H3").GetComponent<TextMeshProUGUI>();
+            TMProCompnt.text = text;
+            TMProCompnt.richText = true;
+
+            newToggle.GetComponent<ToolTip>()._localizableString = tooltip.Localize();
+
+            Listener = listener;
+
+            ButtonCompnt = newToggle.GetComponent<Button>();
+            ButtonCompnt.onClick = new();
+
+            isToggled = defaultState;
+
+            ToggleSprite.sprite = isToggled ? OffSprite : OnSprite;
+
+            ButtonCompnt.onClick.AddListener(new Action(() =>
+            {
+                isToggled = !isToggled;
+
+                ToggleSprite.sprite = isToggled ? OffSprite : OnSprite;
+
+                if (shouldInvoke)
+                    APIBase.SafelyInvolk(isToggled, Listener, text);
+            }));
+
+            newToggle.gameObject.SetActive(true);
+            ToggleSprite.gameObject.SetActive(true);
 
             return this;
         }
 
-        //public void HardSetState(bool value) i hate you cyconi
-        //{
-        //    shouldInvoke = false;
-        //    ToggleCompnt.isOn = value;
-        //    shouldInvoke = true;
+        public void SetToggleState(bool state)
+        {
+            if (isToggled != state)
+            {
+                isToggled = state;
+                ToggleSprite.sprite = isToggled ? OnSprite : OffSprite;
 
-        //    Listener?.Invoke(value);
-        //    ToggleCompnt.onValueChanged.Invoke(value);
-        //}
-        public QMCToggle(QMCGroup group, string text, string tooltip,  Action<bool> stateChange, bool defaultState = false, bool separator = false)
-            : this(group.GetTransform().Find("QM_Settings_Panel/VerticalLayoutGroup").transform, text, tooltip, stateChange, defaultState, separator)
+                if (shouldInvoke)
+                    APIBase.SafelyInvolk(isToggled, Listener, "SoftSet");
+            }
+        }
+
+        public QMCFuncButton(QMCGroup group, string text, string tooltip, Action listener, bool rightContainer = false, bool separator = false, Sprite sprite = null)
+            : this(group.GetTransform().Find("QM_Settings_Panel/VerticalLayoutGroup").transform, text, tooltip, listener, rightContainer, separator, sprite)
         { }
-        public QMCToggle(CollapsibleButtonGroup buttonGroup, string text, string tooltip, Action<bool> stateChange, bool defaultState = false, bool separator = false)
-            : this(buttonGroup.QMCParent, text, tooltip, stateChange, defaultState, separator)
+        public QMCFuncButton(CollapsibleButtonGroup buttonGroup, string text, string tooltip, Action listener, bool rightContainer = false, bool separator = false, Sprite sprite = null)
+            : this(buttonGroup.QMCParent, text, tooltip, listener, rightContainer, separator, sprite)
         { }
     }
 }
