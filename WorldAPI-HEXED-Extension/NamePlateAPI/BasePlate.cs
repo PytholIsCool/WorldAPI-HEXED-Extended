6287c87e6ab5f64f494837700d5a4bc2b518ab23
Pythol <95198306+PytholIsCool@users.noreply.github.com>
2024-12-31 23:30:22 -0600

    Update BasePlate.cs

--- WorldAPI-HEXED-Extension/NamePlateAPI/BasePlate.cs
+++ WorldAPI-HEXED-Extension/NamePlateAPI/BasePlate.cs
@@ -1,4 +1,4 @@
-﻿using TMPro;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using VRC;
@@ -8,7 +8,7 @@ using Object = UnityEngine.Object;
 namespace WorldAPI.NamePlateAPI;
 
 public static class BasePlate {
-    public static Plate AddPlate(this Player player) => new Plate(player);
+    public static Plate AddPlate(this Player player) => new(player);
 
     #region Plates
     public class Plate {
@@ -16,6 +16,8 @@ public static class BasePlate {
         public Transform _basePlate { get; internal set; }
         public Transform transform { get; internal set; }
         public GameObject gameObject { get; internal set; }
+        public Dictionary<string, Tag> TagList { get; internal set; }
+        public Dictionary<Sprite, Icon> IconList { get; internal set; }
         public Plate(Player player) {
             CheckCountContainer(player);
 
@@ -23,17 +25,27 @@ public static class BasePlate {
             transform.DestroyChildren();
 
             if (PlateCount == 0) {
-                transform.parent.Find("Main/Group Banner").localScale = new Vector3(0f, 0f, 0f);
-                transform.parent.Find("Main/Group Banner_Expanded").localScale = new Vector3(0f, 0f, 0f);
-                transform.parent.Find("Group Info").localScale = new Vector3(0f, 0f, 0f);
+                transform.parent.Find("Main/Group Banner").localScale = new(0f, 0f, 0f);
+                transform.parent.Find("Main/Group Banner_Expanded").localScale = new(0f, 0f, 0f);
+                transform.parent.Find("Group Info").localScale = new(0f, 0f, 0f);
             }
             float y = PlateCount * 30f + 60f;
-            transform.localPosition = new Vector3(0, y, 0);
+            transform.localPosition = new(0, y, 0);
 
             gameObject.SetActive(true);
         }
-        public Tag AddTag(string text) => new Tag(this, text);
-        public Icon AddIcon(Sprite sprite, Sprite subSprite = null) => new Icon(this, sprite, subSprite);
+        public Tag AddTag(string text) {
+            var temp = new Tag(this, text);
+            TagList.Add(temp.Identifier, temp);
+            return temp;
+        }
+        public Tag FindTag(string indentifier) => TagList[indentifier];
+        public Icon AddIcon(Sprite sprite, Sprite subSprite = null) {
+            var temp = new Icon(this, sprite, subSprite);
+            IconList.Add(sprite, temp);
+            return temp;
+        }
+        public Icon FindIcon(Sprite identifier) => IconList[identifier];
 
         private static void CheckCountContainer(Player player) { //I've thought of about a million different ways to do this and they'd all be more efficient but ive stopped caring
             if (player._vrcplayer.field_Public_PlayerNameplate_0.transform.Find("Count") == null) {
@@ -53,10 +65,12 @@ public static class BasePlate {
 
         #region Tags
         public class Tag {
+            public string Identifier {  get; private set; }
             public Transform transform { get; internal set; }
             public GameObject gameObject { get; internal set; }
             public TextMeshProUGUI TMProCompt { get; internal set; }
             private string _text { get; set; }
+            private static int TempID = 0;
             public string Text {
                 get => _text; set {
                     _text = value;
@@ -67,7 +81,13 @@ public static class BasePlate {
             public Tag(Plate plate, string text) {
                 (TMProCompt = (transform = (gameObject = Object.Instantiate(plate._basePlate.Find("Contents/Quick Stats/Trust Text"), plate.transform).gameObject).transform).GetComponent<TextMeshProUGUI>()).text = text;
                 TMProCompt.richText = true;
-                TMProCompt.color = new Color(1f, 1f, 1f, 1f);
+                TMProCompt.color = new(1f, 1f, 1f, 1f);
+
+                if (text == null)
+                    Identifier = TempID++.ToString();
+                else
+                    Identifier = text;
+                plate.TagList.Add(Identifier, this);
             }
         }
         #endregion
@@ -100,6 +120,7 @@ public static class BasePlate {
                     (SubImage = Image.transform.Find("Reason").GetComponent<Image>()).overrideSprite = subIcon;
                 else
                     (SubImage = Image.transform.Find("Reason").GetComponent<Image>()).gameObject.SetActive(false);
+                plate.IconList.Add(icon, this);
             }
             #endregion
         }
